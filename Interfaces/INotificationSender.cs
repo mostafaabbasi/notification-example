1c73cf8a253970bdc48504a6feebbec6d2de78e1
mostafaabbasisani <mrcloner.developer@gmail.com>
2022-07-09 15:25:57 +0430

    done

---
+++ Interfaces/INotificationSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NotificationExample.Interfaces
+{
+    public interface INotificationSender
+    {
+        bool IsActive { get; set; }
+
+        void Send(string message);
+
+        void ChangeActiveStatus();
+    }
+}
+
