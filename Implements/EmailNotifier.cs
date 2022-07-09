5fc37fa5686ff0de2850ec2e3ab64c3134b5663a
mostafaabbasisani <mrcloner.developer@gmail.com>
2022-07-09 15:27:27 +0430

    done

---
+++ Implements/EmailNotifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NotificationExample.Interfaces;
+
+namespace NotificationExample.Implements
+{
+    public class EmailNotifier : INotificationSender
+    {
+        public EmailNotifier()
+        {
+            IsActive = true;
+        }
+
+        public bool IsActive { get; set; }
+
+        public void ChangeActiveStatus()
+        {
+            IsActive = !IsActive;
+        }
+
+        public void Send(string message)
+        {
+            Console.WriteLine("email=> " + message);
+        }
+    }
+}
+
---
+++ Implements/SmsNotifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NotificationExample.Interfaces;
+
+namespace NotificationExample.Implements
+{
+    public class SmsNotifier : INotificationSender
+    {
+        public SmsNotifier()
+        {
+            IsActive = true;
+        }
+
+        public bool IsActive { get; set; }
+
+        public void ChangeActiveStatus()
+        {
+            IsActive = !IsActive;
+        }
+
+        public void Send(string message)
+        {
+            Console.WriteLine("sms=> " + message);
+        }
+    }
+}
+
